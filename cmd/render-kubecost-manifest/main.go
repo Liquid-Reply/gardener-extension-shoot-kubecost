167ea55054afddb7e793fab3faa29d3b8849b06a
Tom Gehrke <t.gehrke@reply.de>
2024-10-11 16:33:05 +0200

    fix old type in render-kubecost-manifest

--- cmd/render-kubecost-manifest/main.go
+++ cmd/render-kubecost-manifest/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/liquid-reply/gardener-extension-shoot-kubecost/kubecost"
+	apisconfig "github.com/liquid-reply/gardener-extension-shoot-kubecost/pkg/apis/config"
 )
 
 func main() {
-	out, err := kubecost.Render(kubecost.KubeCostConfig{
-		ApiKey: "my-kubecost-key",
+	out, err := kubecost.Render(&apisconfig.Configuration{
+		ApiToken: "my-kubecost-key",
 	}, true)
 	if err != nil {
 		panic(err)
